f92792baeb16ebba1dcba3093a3b68199f1b2f90
Axe0320 <s2232033hu@chibatech.ac.jp>
2026-04-16 18:21:37 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.15
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    const observeElements = document.querySelectorAll('.scroll-observe');
+    observeElements.forEach(el => observer.observe(el));
+});
